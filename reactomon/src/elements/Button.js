0dbaed603b04cf037871b62e0cd3a648bcd98f3e
Bence <borsi92@gmail.com>
2020-09-26 15:02:33 +0200

    used styled components on the Header

---
+++ reactomon/src/elements/Button.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export default styled.button`
+    height: 30px;
+    background: #2d72b8;
+    border-radius: 10px;
+    border-color: white;
+    &:hover{
+        background: green;
+    }
+`
